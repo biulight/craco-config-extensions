6c569b9aa07231cad557c539371d7616e1e8bb9d
biulight <biulight007@gmail.com>
2023-01-20 10:33:46 +0800

    test: func getType

--- __test__/utils.spec.ts
+++ __test__/utils.spec.ts
@@ -1,9 +1,16 @@
-import { resolveApp } from '../src/utils'
+import { resolveApp, getType } from '../src/utils'
 import { fileURLToPath } from 'node:url'
 
 describe('resolveApp', () => {
   it('should return the correct absolute path', () => {
     const path = resolveApp('src')
     expect(path).toBe(fileURLToPath(new URL('../src', import.meta.url)))
+  })
+})
+
+
+describe('getType', () => {
+  it('target is promise', () => {
+    expect(getType(Promise.resolve())).toBe('promise')
   })
 })

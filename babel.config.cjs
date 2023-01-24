b929d77d9291849003e6fa5c284c75bf2d6697cd
biulight <biulight007@gmail.com>
2023-01-24 15:28:25 +0800

    chore: 修改babel部分插件仅对__test__文件夹转译

---
+++ babel.config.cjs
@@ -0,0 +1,35 @@
+// export default function (api) {
+//   api.cache.using(() => process.env.NODE_ENV)
+//   if (api.env("test")) {
+//     return {
+//       presets: [
+//         ["@babel/preset-env", { targets: { node: "current" } }],
+//         "@babel/preset-typescript",
+//       ],
+//       plugins: [["babel-plugin-transform-import-meta", { module: "ES6" }]],
+//     }
+//   }
+
+//   return {
+//     presets: [
+//       ["@babel/preset-env", { targets: { node: "current" } }],
+//       "@babel/preset-typescript",
+//     ]
+//   }
+// }
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+  // todo 测试下环境变量配置plugin是否可用
+  // test: {
+  //   plugins: [["babel-plugin-transform-import-meta", { module: "ES6" }]],
+  // },
+  overrides: [
+    {
+      test: '__test__',
+      plugins: [["babel-plugin-transform-import-meta", { module: "ES6" }]],
+    }
+  ]
+}

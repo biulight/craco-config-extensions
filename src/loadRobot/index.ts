a01842bdbd9a25b5f8d7be832daf457be5d0eda3
biulight <biulight007@gmail.com>
2023-02-03 19:42:19 +0800

    feat: loadRobot约定STATIC_DOMAIN为静态资源加载域名

--- src/loadRobot/index.ts
+++ src/loadRobot/index.ts
@@ -1,20 +1,25 @@
-
 class EnvConfig {
-
   #envConfig: Record<string, string> = {}
+  #outlet = ["load"]
 
   #proxyHandler = {
     get(target: EnvConfig, propKey: string) {
+      if (target.#outlet.includes(propKey)) return Reflect.get(target, propKey)
       return target.#envConfig[propKey]
       // return Reflect.get(target, propKey)
-    }
+    },
   }
 
   static #permit = false
 
-
-  constructor(options: Record<string, any>, key: string = 'BIU_LIGHT_ENV_CONFIG') {
-    if (!EnvConfig.#permit) throw new Error("EnvConfig only supports instantiation through the static method 'createInstance' ")
+  constructor(
+    options: Record<string, any>,
+    key: string = "BIU_LIGHT_ENV_CONFIG"
+  ) {
+    if (!EnvConfig.#permit)
+      throw new Error(
+        "EnvConfig only supports instantiation through the static method 'createInstance' "
+      )
     // 初始化
     this.#init(options)
     // 挂载
@@ -25,10 +30,13 @@ class EnvConfig {
     return window[key]
   }
 
-  /** 
-  * create instance
-  */
-  static createInstance(options: Record<string, any>, key = 'PAB_BANK_ENV_CONFIG') {
+  /**
+   * create instance
+   */
+  static createInstance(
+    options: Record<string, any>,
+    key = "PAB_BANK_ENV_CONFIG"
+  ) {
     // @ts-ignore
     if (window[key]) return window[key]
     this.#permit = true
@@ -38,27 +46,43 @@ class EnvConfig {
   #init(options: Record<string, any>) {
     const { hostname } = new URL(window.location.href)
     this.#envConfig = options[hostname] || {}
-    console.log(import.meta, 'meta')
-    console.log(import.meta.url, '=====url====')
-    console.log(document.currentScript, 'document.currentScript')
+    const staticDomain = this.#envConfig["STATIC_DOMAIN"]
+    this.createBase(staticDomain)
+    console.log(import.meta, "meta>>>211>>>info")
+    console.log(import.meta.url, "=====url====")
+    console.log(document.currentScript, "document.currentScript")
     console.log("this.#envConfig", this.#envConfig)
   }
   /**
-  * load baseUrl by creating base element
-  *
-  */
-  load(callback: (that: EnvConfig) => string, success?: () => void) {
-    const url = callback(this)
-    let baseEle = document.createElement('base')
+   * load baseUrl by creating base element
+   *
+   */
+  createBase(url?: string, success?: () => void) {
+    if (!url) return
+    let baseEle = document.createElement("base")
     baseEle.href = url
 
-    const header = document.getElementsByTagName('head')[0]
+    const header = document.getElementsByTagName("head")[0]
+    // header.insertBefore(baseEle, document.currentScript!.nextElementSibling)
+    header.insertBefore(baseEle, header.firstElementChild)
+    // load successfully
+    success && success()
+  }
+  /**
+   * load baseUrl by creating base element
+   *
+   */
+  load(callback: (that: EnvConfig) => string, success?: () => void) {
+    const url = callback(this)
+    let baseEle = document.createElement("base")
+    baseEle.href = url
+
+    const header = document.getElementsByTagName("head")[0]
     header.insertBefore(baseEle, document.currentScript!.nextElementSibling)
     // load successfully
     success && success()
   }
 }
-
 
 export default EnvConfig
 // export default EnvConfig.createInstance({

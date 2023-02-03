2480b9ff5078eaed6ae648e8e0a46860dd1009f4
biulight <biulight007@gmail.com>
2023-02-04 00:46:56 +0800

    feat: 增加htmlWebpackInjectHead插件

---
+++ src/utilities/htmlWebpackInjectHead.ts
@@ -0,0 +1,44 @@
+import type { Compiler } from "webpack"
+import type HtmlWebpackPlugin from "html-webpack-plugin"
+import { insertStringBefore, insertStringAfter } from "@/utils"
+
+interface Options {
+  content: string
+  position?: "start" | "end"
+}
+
+export default class HtmlWebpackInjectHead {
+  options: Options
+  htmlWebpackPlugin: typeof HtmlWebpackPlugin
+
+  constructor(htmlWebpackPlugin: typeof HtmlWebpackPlugin, options: Options) {
+    this.htmlWebpackPlugin = htmlWebpackPlugin
+    this.options = {
+      position: "start",
+      ...options,
+    }
+  }
+
+  apply(compiler: Compiler) {
+    compiler.hooks.compilation.tap("HtmlWebpackInjectHead", (compilation) => {
+      this.htmlWebpackPlugin
+        .getHooks(compilation)
+        .beforeEmit.tap("HtmlWebpackInjectHead", (data) => {
+          if (this.options.position === "end") {
+            data.html = insertStringBefore(
+              data.html,
+              "</head>",
+              this.options.content
+            )
+          } else {
+            data.html = insertStringAfter(
+              data.html,
+              "<head>",
+              this.options.content
+            )
+          }
+          return data
+        })
+    })
+  }
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,72 @@
 import path from "node:path"
-import fs, { PathLike } from "node:fs"
+import fs from "node:fs"
 import type { Configuration } from "webpack"
 
 const appDirectory = fs.realpathSync(process.cwd())
 
 /** load file */
-export const resolveApp = (relativePath: string) => path.resolve(appDirectory, relativePath)
+export const resolveApp = (relativePath: string) =>
+  path.resolve(appDirectory, relativePath)
 
 /** look for webpack plugin that you expect */
-export const getWebpackPlugin = (plugins: Configuration['plugins'] = [], pluginName: string, id: number = 0) => {
-  if (id === 0) return plugins.find((plugin) => plugin.constructor.name === pluginName)
-  // return plugins.filter((plugin) => plugin.constructor.name === pluginName)[id] || throw new Error("Function getWebpackPlugin")
-  const sets = plugins.filter((plugin) => plugin.constructor.name === pluginName)
-  if (id > sets.length) throw new Error('Function getWebpackPlugin')
+export const getWebpackPlugin = (
+  plugins: Configuration["plugins"] = [],
+  pluginName: string,
+  id: number = 0
+) => {
+  if (id === 0)
+    return plugins.find((plugin) => plugin.constructor.name === pluginName)
+  const sets = plugins.filter(
+    (plugin) => plugin.constructor.name === pluginName
+  )
+  if (id > sets.length) throw new Error("Function getWebpackPlugin")
   return sets[id]
 }
 
-
 /** get target type */
-export const getType = (target:unknown) => {
+export const getType = (target: unknown) => {
   return Object.prototype.toString.call(target).slice(8, -1).toLocaleLowerCase()
 }
 
+/**
+ *
+ * @param haystack
+ * @param needle inserted position
+ * @param insertedString
+ * @returns {string} concatenated string
+ */
+export const insertStringAfter = (
+  haystack: string,
+  needle: string,
+  insertedString: string
+) => {
+  if (!insertedString) return haystack
+  const needleString = String(needle)
+  const index = haystack.indexOf(needleString)
+  if (index === -1) {
+    return haystack
+  }
+  const endIndex = index + needleString.length
+  return haystack.slice(0, endIndex) + insertedString + haystack.slice(endIndex)
+}
+
+/**
+ *
+ * @param haystack
+ * @param needle inserted position
+ * @param insertedString
+ * @returns {string} concatenated string
+ */
+export const insertStringBefore = (
+  haystack: string,
+  needle: string,
+  insertedString: string
+) => {
+  if (!insertedString) return haystack
+  const needleString = String(needle)
+  const index = haystack.indexOf(needleString)
+  if (index === -1) {
+    return haystack
+  }
+  return haystack.slice(0, index) + insertedString + haystack.slice(index)
+}

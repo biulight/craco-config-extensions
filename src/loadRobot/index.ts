da5401d8bb1b44262eb9d4c73796f10cc14012fd
biulight <biulight007@gmail.com>
2023-02-17 22:34:43 +0800

    feat: 优化动态加载函数并增加支持动态创建资源标签

--- src/loadRobot/index.ts
+++ src/loadRobot/index.ts
@@ -1,9 +1,9 @@
-class EnvConfig {
+class LoadRobot {
   #envConfig: Record<string, string> = {}
   #outlet = ["load"]
 
   #proxyHandler = {
-    get(target: EnvConfig, propKey: string) {
+    get(target: LoadRobot, propKey: string) {
       if (target.#outlet.includes(propKey)) return Reflect.get(target, propKey)
       return target.#envConfig[propKey]
       // return Reflect.get(target, propKey)
@@ -16,16 +16,16 @@ class EnvConfig {
     options: Record<string, any>,
     key: string = "BIU_LIGHT_ENV_CONFIG"
   ) {
-    if (!EnvConfig.#permit)
+    if (!LoadRobot.#permit)
       throw new Error(
-        "EnvConfig only supports instantiation through the static method 'createInstance' "
+        "LoadRobot only supports instantiation through the static method 'createInstance' "
       )
     // 初始化
     this.#init(options)
     // 挂载
     // @ts-ignore
     window[key] = new Proxy(this, this.#proxyHandler)
-    EnvConfig.#permit = false
+    LoadRobot.#permit = false
     // @ts-ignore
     return window[key]
   }
@@ -40,7 +40,7 @@ class EnvConfig {
     // @ts-ignore
     if (window[key]) return window[key]
     this.#permit = true
-    return new EnvConfig(options, key)
+    return new LoadRobot(options, key)
   }
 
   #init(options: Record<string, any>) {
@@ -69,22 +69,52 @@ class EnvConfig {
     success && success()
   }
   /**
-   * load baseUrl by creating base element
+   * load resource by creating tag element
    *
    */
-  load(callback: (that: EnvConfig) => string, success?: () => void) {
-    const url = callback(this)
-    let baseEle = document.createElement("base")
-    baseEle.href = url
+  static load(list: string[]) {
+    for (const url of list) {
+      if (url.endsWith(".js")) LoadRobot.createScript(url)
+      if (url.endsWith(".css")) LoadRobot.createLink(url)
+    }
+  }
 
-    const header = document.getElementsByTagName("head")[0]
-    header.insertBefore(baseEle, document.currentScript!.nextElementSibling)
-    // load successfully
-    success && success()
+  static createScript(url: string, callback?: Function) {
+    let script = document.createElement("script")
+    let fn = callback || function () {}
+    script.setAttribute("type", "text/javascript")
+    // @ts-ignore
+    if (script.readyState) {
+      // @ts-ignore
+      script.onreadystatechange = function () {
+        // @ts-ignore
+        if (script.readyState == "loaded" || script.readyState == "complete") {
+          // @ts-ignore
+          script.onreadystatechange = null
+          fn()
+        }
+      }
+    } else {
+      // 其他现代浏览器
+      script.onload = function () {
+        fn()
+      }
+    }
+
+    script.src = url
+    document.getElementsByTagName("head")[0].appendChild(script)
+  }
+
+  static createLink(url: string) {
+    const link = document.createElement("link")
+    link.setAttribute("rel", "stylesheet")
+    link.setAttribute("type", "text/css")
+    link.setAttribute("href", url)
+    document.getElementsByTagName("head")[0].appendChild(link)
   }
 }
 
-export default EnvConfig
-// export default EnvConfig.createInstance({
+export default LoadRobot
+// export default LoadRobot.createInstance({
 //   ...process.env.DYNAMIC_ENV
 // })
